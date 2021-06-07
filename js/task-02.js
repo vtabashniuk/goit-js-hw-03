c95ca5a9fa255e175c2cea9931d6ae8ee312fe45
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-06-07 22:51:35 +0300

    добавляет решение Задачи - 2

---
+++ js/task-02.js
@@ -0,0 +1,26 @@
+/*
+Напиши функцию countProps(obj), считающую кол-во свойств в объекте. Функция возвращает число - количество свойств.
+
+const countProps = function(obj) {
+  // твой код
+};
+
+/////
+/// Вызовы функции для проверки работоспособности твоей реализации.
+/////
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+*/
+
+const countProps = function (obj) {
+    return Object.keys(obj).length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
