158c064dc7494198d7074d7ca69f830395fb40f5
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-06-13 00:55:30 +0300

    добавляет решение Задачи - 5

---
+++ js/task-05.js
@@ -0,0 +1,52 @@
+/* Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства. Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function(arr, prop) {
+  // твой код
+};
+
+/////
+// Вызовы функции для проверки работоспособности твоей реализации.
+/////
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // [] */
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  let arrayOfValues = [];
+
+  //     for (let i = 0; i < arr.length; i += 1) {
+  //         if (Object.keys(arr[i]).includes(prop)) {
+  //             arrayOfValues.push(arr[i][prop]);
+  //     }
+  //   }
+
+  for (const element of arr) {
+      if (Object.keys(element).includes(prop)) {
+          arrayOfValues.push(element[prop]);
+      } else return `Свойство \"${prop}\" отсутсвует в данных объектах.`;
+  }
+
+  return arrayOfValues;
+};
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // [] */
