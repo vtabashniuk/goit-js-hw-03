a115faaf4cbcea650d7f4f5a789767c41d7322a0
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-06-12 18:31:02 +0300

    добавляет решение Задачи - 4

---
+++ js/task-04.js
@@ -0,0 +1,57 @@
+/* Напиши функцию countTotalSalary(employees) принимающую объект зарплат. Функция считает общую сумму запрплаты работников и возращает ее. Каждое поле объекта, передаваемого в функцию, имеет вид "имя":"зарплата".
+
+const countTotalSalary = function(employees) {
+  // твой код
+};
+
+///////
+/// Вызовы функции для проверки работоспособности твоей реализации.
+///////
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
+
+*/
+
+const countTotalSalary = function (employees) {
+  const listOfEmployeer = [...Object.values(employees)];
+  let totalSalary = 0;
+
+  for (const salary of listOfEmployeer) {
+    totalSalary += salary;
+  }
+  return totalSalary;
+};
+
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
